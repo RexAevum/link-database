967e06f6a33249a73561e9dad3867e113a4fe55d
RexAevum <rolans2k@gmil.com>
2020-07-07 17:48:31 -0400

    Initial Commit

---
+++ Rapin001P5/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rapin001P5
+//
+//  Created by Rolans Apinis on 7/7/20.
+//  Copyright © 2020 Rolans Apinis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
